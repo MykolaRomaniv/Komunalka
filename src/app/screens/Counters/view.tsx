08a4c26719c8e00274de8c22640891c738cc8c65
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-05-13 21:07:50 +0300

    feat(Counters): finixh layout

--- src/app/screens/Counters/view.tsx
+++ src/app/screens/Counters/view.tsx
@@ -1,14 +1,29 @@
+import arrowDown from 'assets/icons/arrowDown.png'
+import calendar from 'assets/icons/calendar.png'
 import Header from 'common/Header'
+import PaymentSection from 'common/PaymentSection'
+import CountersItem from 'components/CountersItem'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 
 import styles from './styles'
 
 const ContentView = () => (
-  <View style={styles.page}>
+  <ScrollView style={styles.page}>
     <Header>{'Показники лічильників'}</Header>
-    <Text />
-  </View>
+    <PaymentSection />
+    <Text style={styles.street}>{'Вулиця Лукаша, 54, 8'}</Text>
+    <View style={styles.monthContainer}>
+      <Image source={calendar} style={styles.calendarIcon} />
+      <Text>{'Квітень 2021'}</Text>
+      <Image source={arrowDown} style={styles.arrowDown} />
+    </View>
+    <CountersItem />
+    <CountersItem />
+    <CountersItem />
+    <CountersItem />
+  </ScrollView>
 )
 
 export default ContentView
