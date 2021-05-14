068dcb9ca7caebcb7ba95de81735fdd9d199711a
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-05-14 23:57:17 +0300

    feat(Statistic): add basic layout

--- src/app/screens/Statistic/styles.ts
+++ src/app/screens/Statistic/styles.ts
@@ -1,10 +1,92 @@
 import colors from 'constants/colors'
+import fonts from 'constants/fonts'
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
   page: {
     height: '100%',
-    backgroundColor: colors.white,
+  },
+  graphContentWrapper: {
+    backgroundColor: colors.charade,
+    height: '50%',
+  },
+  rowTitle: {
+    fontFamily: 'Source Sans Pro',
+    fontStyle: 'italic',
+    fontWeight: '200',
+    fontSize: 12,
+    lineHeight: 15,
+    color: colors.white,
+    marginVertical: 5,
+    marginLeft: 19,
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 19,
+  },
+  bgGraph: {
+    height: '30%',
+    alignSelf: 'flex-end',
+  },
+  banner: {
+    backgroundColor: colors.romance,
+    // box-shadow: 0px 20px 50px rgba(0, 0, 0, 0.15),
+    elevation: 3,
+    borderRadius: 45,
+    marginTop: -45,
+    paddingBottom: 45,
+  },
+  month: {
+    ...fonts.medium,
+    fontSize: 12,
+    lineHeight: 14,
+    color: colors.black,
+    marginLeft: 19,
+    marginTop: 24,
+  },
+  horizontalDivider: {
+    opacity: 0.3,
+    backgroundColor: colors.black,
+    height: 1,
+    width: '100%',
+  },
+  serviceType: {
+    ...fonts.bold,
+    fontSize: 24,
+    lineHeight: 28,
+    color: colors.black,
+    marginLeft: 19,
+    marginTop: 27,
+  },
+  amount: {
+    ...fonts.medium,
+    fontSize: 48,
+    lineHeight: 57,
+    color: colors.firefly,
+    marginTop: 10,
+  },
+  currency: {
+    ...fonts.bold,
+    fontSize: 24,
+    lineHeight: 28,
+    color: colors.firefly,
+    marginTop: 31,
+    marginRight: 26,
+  },
+  difference: {
+    ...fonts.light,
+    fontSize: 18,
+    lineHeight: 21,
+    color: colors.silverChalice,
+  },
+  serviceRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  amountRow: {
+    flexDirection: 'row',
+    // alignItems: 'flex-end',
   },
 })
 
--- src/app/screens/Statistic/view.tsx
+++ src/app/screens/Statistic/view.tsx
@@ -1,12 +1,45 @@
+// import bgGraph from 'assets/bgGraph.png'
 import Header from 'common/Header'
+import StatisticButton from 'components/StatisticButton'
 import React from 'react'
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
 
 import styles from './styles'
 
 const ContentView = () => (
   <View style={styles.page}>
     <Header>{'Статистика'}</Header>
+    <View style={styles.graphContentWrapper}>
+      <Text style={styles.rowTitle}>{'Комунальні послуги:'}</Text>
+      <View style={styles.buttonRow}>
+        <StatisticButton>{'Вода'}</StatisticButton>
+        <StatisticButton selected>{'Світло'}</StatisticButton>
+        <StatisticButton>{'Опалення'}</StatisticButton>
+      </View>
+      <Text style={styles.rowTitle}>{'Період:'}</Text>
+      <View style={styles.buttonRow}>
+        <StatisticButton>{'7 днів'}</StatisticButton>
+        <StatisticButton selected>{'31 день'}</StatisticButton>
+        <StatisticButton>{'12 місцяів'}</StatisticButton>
+      </View>
+      {/* <Image source={bgGraph} style={styles.bgGraph} /> */}
+    </View>
+    <View style={styles.banner}>
+      <Text style={styles.month}>{'Березень 2021'}</Text>
+      <View style={styles.horizontalDivider} />
+      <View style={styles.serviceRow}>
+        <Text style={styles.serviceType}>{'Вода'}</Text>
+        <View />
+        <View>
+          <View style={styles.amountRow}>
+            <Text style={styles.amount}>{'233'}</Text>
+            <Text style={styles.currency}>{'ГРН'}</Text>
+          </View>
+          <Text style={styles.difference}>{'Підвищення 10%'}</Text>
+        </View>
+      </View>
+      <View />
+    </View>
   </View>
 )
 
