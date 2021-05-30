832502aaabdc55f55cbdfa63341d4a67b224db9b
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-05-30 23:39:32 +0300

    feat: add swipable list component

---
+++ src/app/common/SwipableList/styles.ts
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  backTextWhite: {
+    color: '#FFF',
+  },
+  rowFront: {
+    alignItems: 'center',
+    backgroundColor: '#CCC',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+    height: 50,
+  },
+  rowBack: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 15,
+    borderRadius: 45,
+  },
+  backRightBtn: {
+    alignItems: 'center',
+    bottom: 0,
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    width: 75,
+  },
+  backRightBtnRight: {
+    backgroundColor: '#FF1744',
+    right: 1,
+    borderTopRightRadius: 45,
+    borderBottomRightRadius: 45,
+    marginBottom: 20,
+  },
+})
+
+export default styles
