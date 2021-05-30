72e77a42aaf7fa8c7da50c77e44d11cb760c90fa
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-05-30 23:39:47 +0300

    refactor(Addresses): using swipable list

--- src/app/screens/Addresses/view.tsx
+++ src/app/screens/Addresses/view.tsx
@@ -1,13 +1,41 @@
 import add from 'assets/icons/add.png'
 import mapBg from 'assets/mapBg.png'
 import Header from 'common/Header'
+import SwipableList from 'common/SwipableList'
 import AddressItem from 'components/AddressItem'
 import Touchable from 'components/Touchable'
 import React from 'react'
 import { Image, View } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler'
+import { IAddressItem } from 'types'
 
 import styles from './styles'
+
+const ADDRESSES: IAddressItem[] = [
+  {
+    address: 'Вулиця Лукаша, 54, 6',
+    city: 'м. Львів',
+  },
+  {
+    address: 'Вулиця Лукаша, 54, 7',
+    city: 'м. Львів',
+  },
+  {
+    address: 'Вулиця Лукаша, 54, 8',
+    city: 'м. Львів',
+  },
+  {
+    address: 'Вулиця Лукаша, 54, 9',
+    city: 'м. Львів',
+  },
+  {
+    address: 'Вулиця Лукаша, 54, 10',
+    city: 'м. Львів',
+  },
+  {
+    address: 'Вулиця Лукаша, 54, 11',
+    city: 'м. Львів',
+  },
+]
 
 const ContentView = () => (
   <View style={styles.page}>
@@ -18,13 +46,15 @@ const ContentView = () => (
         <Image source={add} style={styles.addIcon} />
       </Touchable>
     </View>
-    <ScrollView style={styles.scrollable}>
-      <AddressItem address="Вулиця Лукаша, 54, 8" city="м. Львів" />
-      <AddressItem address="Вулиця Лукаша, 54, 8" city="м. Львів" />
-      <AddressItem address="Вулиця Лукаша, 54, 8" city="м. Львів" />
-      <AddressItem address="Вулиця Лукаша, 54, 8" city="м. Львів" />
-      <AddressItem address="Вулиця Лукаша, 54, 8" city="м. Львів" />
-    </ScrollView>
+    <SwipableList
+      data={ADDRESSES.map((address) => ({
+        ...address,
+        key: address.address,
+      }))}
+      renderItem={(item) => (
+        <AddressItem address={item.item.address} city={item.item.city} />
+      )}
+    />
   </View>
 )
 
