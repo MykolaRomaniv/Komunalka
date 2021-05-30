0fd9764071195f53c606e566a10da90a2b7f6114
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-05-30 19:53:05 +0300

    fix(AddressItem): remove selected prop

--- src/app/components/AddressItem/index.tsx
+++ src/app/components/AddressItem/index.tsx
@@ -1,5 +1,4 @@
-import addressIcon from 'assets/icons/address.png'
-import selectedAddress from 'assets/icons/selectedAddress.png'
+import selectedAddress from 'assets/icons/mapPinSelected.png'
 import Touchable from 'components/Touchable'
 import React from 'react'
 import { Image, Text, View } from 'react-native'
@@ -7,17 +6,13 @@ import { Image, Text, View } from 'react-native'
 import styles from './styles'
 
 interface AddressItemProps {
-  selected?: boolean
   address: string
   city: string
 }
 
-const AddressItem = ({ selected, address, city }: AddressItemProps) => (
+const AddressItem = ({ address, city }: AddressItemProps) => (
   <Touchable style={styles.container} activeOpacity={0.87}>
-    <Image
-      source={selected ? selectedAddress : addressIcon}
-      style={styles.icon}
-    />
+    <Image source={selectedAddress} style={styles.icon} />
     <View>
       <Text style={styles.addressText}>{address}</Text>
       <Text style={styles.cityText}>{city}</Text>
