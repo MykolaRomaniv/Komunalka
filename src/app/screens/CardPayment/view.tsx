fd9f02ca3a40175aa123873513660d5ea6a811b8
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-05-30 19:56:38 +0300

    refactor(CardPayment): use Button component

--- src/app/screens/CardPayment/view.tsx
+++ src/app/screens/CardPayment/view.tsx
@@ -1,6 +1,6 @@
 import Header from 'common/Header'
 import PaymentSection from 'common/PaymentSection'
-import Touchable from 'components/Touchable'
+import Button from 'components/Button'
 import React from 'react'
 import { Text, View } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
@@ -25,13 +25,7 @@ const ContentView = ({ save, valid, sum }: ContentViewProps) => (
       <View style={styles.content}>
         <Text style={styles.title}>{'Платіжна картка:'}</Text>
         <Form />
-        <Touchable
-          onPress={save}
-          disabled={!valid}
-          style={[styles.submitButton, !valid && styles.disabledButton]}
-        >
-          <Text style={styles.submitText}>{`Сплатити ${sum}`}</Text>
-        </Touchable>
+        <Button disabled={!valid} onPress={save}>{`Сплатити ${sum}`}</Button>
       </View>
     </View>
   </KeyboardAwareScrollView>
