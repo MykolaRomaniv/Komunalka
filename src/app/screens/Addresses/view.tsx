936429a317edcfeb147b007023612b16612a2955
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-05-30 19:55:48 +0300

    feat(Address): rewrite to correspond to new design

--- src/app/screens/Addresses/view.tsx
+++ src/app/screens/Addresses/view.tsx
@@ -1,32 +1,31 @@
-import addressesBg from 'assets/addressesBg.png'
 import add from 'assets/icons/add.png'
-import trash from 'assets/icons/trash.png'
+import mapBg from 'assets/mapBg.png'
 import Header from 'common/Header'
 import AddressItem from 'components/AddressItem'
 import Touchable from 'components/Touchable'
 import React from 'react'
-import { Image, ImageBackground, View } from 'react-native'
+import { Image, View } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 
 import styles from './styles'
 
 const ContentView = () => (
-  <ScrollView style={styles.page}>
+  <View style={styles.page}>
     <Header>{'Адреси'}</Header>
-    <ImageBackground source={addressesBg} style={styles.background}>
-      <View style={styles.iconsRow}>
-        <Touchable>
-          <Image source={trash} style={styles.trashIcon} />
-        </Touchable>
-        <Touchable>
-          <Image source={add} style={styles.addIcon} />
-        </Touchable>
-      </View>
-      <AddressItem address="Вулиця Лукаша, 54, 8" city="м. Львів" selected />
+    <Image source={mapBg} style={styles.map} />
+    <View style={styles.iconsRow}>
+      <Touchable>
+        <Image source={add} style={styles.addIcon} />
+      </Touchable>
+    </View>
+    <ScrollView style={styles.scrollable}>
       <AddressItem address="Вулиця Лукаша, 54, 8" city="м. Львів" />
       <AddressItem address="Вулиця Лукаша, 54, 8" city="м. Львів" />
-    </ImageBackground>
-  </ScrollView>
+      <AddressItem address="Вулиця Лукаша, 54, 8" city="м. Львів" />
+      <AddressItem address="Вулиця Лукаша, 54, 8" city="м. Львів" />
+      <AddressItem address="Вулиця Лукаша, 54, 8" city="м. Львів" />
+    </ScrollView>
+  </View>
 )
 
 export default ContentView
