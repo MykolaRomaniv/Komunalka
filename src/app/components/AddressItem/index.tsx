c547407bf2898c16a8ed4b519141323a3b91fbb2
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-06-01 00:56:36 +0300

    fix(AddressItem): using correct props

--- src/app/components/AddressItem/index.tsx
+++ src/app/components/AddressItem/index.tsx
@@ -1,19 +1,23 @@
 import selectedAddress from 'assets/icons/mapPinSelected.png'
 import React from 'react'
 import { Image, Text, View } from 'react-native'
+import { IAddressItem } from 'types'
 
 import styles from './styles'
 
 interface AddressItemProps {
-  address: string
-  city: string
+  item: IAddressItem
 }
 
-const AddressItem = ({ address, city }: AddressItemProps) => (
+const AddressItem = ({
+  item: { city, street, homeNumber, corpus },
+}: AddressItemProps) => (
   <View style={styles.container}>
     <Image source={selectedAddress} style={styles.icon} />
     <View>
-      <Text style={styles.addressText}>{address}</Text>
+      <Text style={styles.addressText}>
+        {`Вулиця ${street}, ${homeNumber}, ${corpus}`}
+      </Text>
       <Text style={styles.cityText}>{city}</Text>
     </View>
   </View>
