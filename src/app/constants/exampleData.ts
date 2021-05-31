76b5001ab98124dbe977192ca0241beb6dbf5a0c
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-05-31 23:08:41 +0300

    feat(constants): add addresses and counter items

---
+++ src/app/constants/exampleData.ts
@@ -0,0 +1,59 @@
+import { IAddressItem, ICounterItem } from 'types'
+
+export const ADDRESSES: IAddressItem[] = [
+  // {
+  //   address: 'Вулиця Лукаша, 54, 6',
+  //   city: 'м. Львів',
+  // },
+  // {
+  //   address: 'Вулиця Лукаша, 54, 7',
+  //   city: 'м. Львів',
+  // },
+  // {
+  //   address: 'Вулиця Лукаша, 54, 8',
+  //   city: 'м. Львів',
+  // },
+  // {
+  //   address: 'Вулиця Лукаша, 54, 9',
+  //   city: 'м. Львів',
+  // },
+  // {
+  //   address: 'Вулиця Лукаша, 54, 10',
+  //   city: 'м. Львів',
+  // },
+  // {
+  //   address: 'Вулиця Лукаша, 54, 11',
+  //   city: 'м. Львів',
+  // },
+]
+
+export const COUNTERS: ICounterItem[] = [
+  {
+    companyName: 'КП “Львівводоканал”',
+    utilityType: 'хол. вода і відведення',
+    personalAccount: '670000080671',
+    counterState: 54500,
+    newCounterState: 54617,
+  },
+  {
+    companyName: 'КП “Львівводоканал”',
+    utilityType: 'хол. вода і відведення',
+    personalAccount: '670000080672',
+    counterState: 54500,
+    newCounterState: 54617,
+  },
+  {
+    companyName: 'КП “Львівводоканал”',
+    utilityType: 'хол. вода і відведення',
+    personalAccount: '670000080673',
+    counterState: 54500,
+    newCounterState: 54617,
+  },
+  {
+    companyName: 'КП “Львівводоканал”',
+    utilityType: 'хол. вода і відведення',
+    personalAccount: '670000080674',
+    counterState: 54500,
+    newCounterState: 54617,
+  },
+]
