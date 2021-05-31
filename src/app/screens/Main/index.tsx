6ae59bc22c4c12ad0ee483bd33f6edf9e21b21bf
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-05-31 23:07:32 +0300

    feat(Main): get user name from storage

--- src/app/screens/Main/index.tsx
+++ src/app/screens/Main/index.tsx
@@ -3,7 +3,8 @@ import FooterMenu from 'common/FooterMenu'
 import Header from 'common/Header'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { AppStackParamList } from 'types'
+import { useSelector } from 'react-redux'
+import { AppStackParamList, ReduxState } from 'types'
 
 import ContentView from './view'
 
@@ -11,12 +12,18 @@ interface MainProps {
   navigation: StackNavigationProp<AppStackParamList, 'Main'>
 }
 
-const Main = ({ navigation }: MainProps) => (
-  <SafeAreaView style={{ flex: 1 }}>
-    <Header navigation={navigation}>{'Привіт, Педро'}</Header>
-    <ContentView navigation={navigation} />
-    <FooterMenu navigation={navigation} />
-  </SafeAreaView>
-)
+const Main = ({ navigation }: MainProps) => {
+  const userName = useSelector(
+    (state: ReduxState) => state.user.userData?.user.name,
+  )
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Header navigation={navigation}>{`Привіт, ${userName}`}</Header>
+      <ContentView navigation={navigation} />
+      <FooterMenu navigation={navigation} />
+    </SafeAreaView>
+  )
+}
 
 export default Main
