200af6c30c21c010ebed96301cca531620d73d70
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-06-01 00:55:18 +0300

    fix(Header): revert position absolute

--- src/app/common/Header/styles.ts
+++ src/app/common/Header/styles.ts
@@ -8,7 +8,8 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingBottom: 10,
     backgroundColor: '#fff',
-    position: 'absolute',
+    // TODO
+    // position: 'absolute',
     top: 0,
     zIndex: 1,
     width: '100%',
