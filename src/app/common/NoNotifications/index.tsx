b32f782576c418900726d995ff6b7b8ad4ba7480
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-06-09 22:22:14 +0300

    feat(NoNotification): add onButtonPress props

--- src/app/common/NoNotifications/index.tsx
+++ src/app/common/NoNotifications/index.tsx
@@ -4,11 +4,15 @@ import { Text, View } from 'react-native'
 
 import styles from './styles'
 
-const NoNotifications = () => (
+interface NoNotificationsProps {
+  onButtonPress: () => void
+}
+
+const NoNotifications = ({ onButtonPress }: NoNotificationsProps) => (
   <View style={styles.content}>
     <Text style={styles.text}>{'У вас ще немає жодного нагадування'}</Text>
     <View style={styles.buttonWrapper}>
-      <Button>{'Створити'}</Button>
+      <Button onPress={onButtonPress}>{'Створити'}</Button>
     </View>
   </View>
 )
