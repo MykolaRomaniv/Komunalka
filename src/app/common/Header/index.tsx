ba337762ffa6ee9d6ebf98a0ae0fad0e61d75e01
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-06-10 15:56:07 +0300

    feat(Header): initials logic

--- src/app/common/Header/index.tsx
+++ src/app/common/Header/index.tsx
@@ -1,8 +1,9 @@
 import { StackNavigationProp } from '@react-navigation/stack'
 import UserCircle from 'components/UserCircle'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text, View } from 'react-native'
-import { AppStackParamList } from 'types'
+import { useSelector } from 'react-redux'
+import { AppStackParamList, ReduxState } from 'types'
 
 import styles from './styles'
 
@@ -11,11 +12,28 @@ interface HeaderProps {
   navigation: StackNavigationProp<AppStackParamList, keyof AppStackParamList>
 }
 
-const Header = ({ children, navigation: { navigate } }: HeaderProps) => (
-  <View style={styles.header}>
-    <Text style={styles.headerText}>{children}</Text>
-    <UserCircle onPress={() => navigate('Registration')} />
-  </View>
-)
+const Header = ({ children, navigation: { navigate } }: HeaderProps) => {
+  const user = useSelector((state: ReduxState) => state.user.userData?.user)
+  const [userInitials, setUserInitials] = useState<string>()
+
+  useEffect(() => {
+    user &&
+      user.name &&
+      user.familyName &&
+      setUserInitials(
+        `${user.name[0].toUpperCase()}${user.familyName[0].toUpperCase()}`,
+      )
+  }, [user])
+
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerText}>{children}</Text>
+      <UserCircle
+        onPress={() => navigate('Registration')}
+        userInitials={userInitials}
+      />
+    </View>
+  )
+}
 
 export default Header
